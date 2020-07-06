a6dd0bf38f9b0c81db21966cf72ab4ed58fa68ff
Mathias Top <mtop@localhost.localdomain>
2020-07-06 15:46:30 +0200

    [+] disciplines' controller and templates

--- src/Controller/DisciplinesController.php
+++ src/Controller/DisciplinesController.php
@@ -16,4 +16,44 @@ class DisciplinesController extends AbstractController
             'controller_name' => 'DisciplinesController',
         ]);
     }
+
+    /**
+     * @Route("/judo", name="judo")
+     */
+    public function indexJudo()
+    {
+        return $this->render('disciplines/judo.html.twig', [
+            'controller_name' => 'JudoController',
+        ]);
+    }
+
+    /**
+     * @Route("/ju-jitsu", name="ju-jitsu")
+     */
+    public function indexJuJitsu()
+    {
+        return $this->render('disciplines/ju-jitsu.html.twig', [
+            'controller_name' => 'JuJitsuController',
+        ]);
+    }
+
+    /**
+     * @Route("/taiso", name="taiso")
+     */
+    public function indexTaiso()
+    {
+        return $this->render('disciplines/taiso.html.twig', [
+            'controller_name' => 'TaisoController',
+        ]);
+    }
+
+    /**
+     * @Route("/ne-waza", name="ne-waza")
+     */
+    public function indexNeWaza()
+    {
+        return $this->render('disciplines/ne-waza.html.twig', [
+            'controller_name' => 'NeWazaController',
+        ]);
+    }
 }
