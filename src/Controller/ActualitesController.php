488264dcc3201b200feae2878dbbdee3686c7482
Mathias Top <mtop@localhost.localdomain>
2020-07-10 18:13:06 +0200

    [+] actualités' controller and templates

--- src/Controller/ActualitesController.php
+++ src/Controller/ActualitesController.php
@@ -16,4 +16,34 @@ class ActualitesController extends AbstractController
             'controller_name' => 'ActualitesController',
         ]);
     }
+
+    /**
+     * @Route("/newsletter", name="newsletter")
+     */
+    public function indexNewletter()
+    {
+        return $this->render('actualites/newsletter.html.twig', [
+            'controller_name' => 'NewsletterController',
+        ]);
+    }
+
+    /**
+     * @Route("/actualites-club", name="actualites-club")
+     */
+    public function indexActualitesClub()
+    {
+        return $this->render('actualites/actualites-club.html.twig', [
+            'controller_name' => 'ActualitesClubController',
+        ]);
+    }
+
+    /**
+     * @Route("/actualites-nationale-internationale", name="actualites-nationale-internationale")
+     */
+    public function indexActualitesNationaleInternationale()
+    {
+        return $this->render('actualites/actualites-nationale-internationale.html.twig', [
+            'controller_name' => 'ActualitesNationaleInternationaleClubController',
+        ]);
+    }
 }
