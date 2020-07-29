4e2f7a41635a24cc25928000398cd77e54081060
Mathias Top <mtop@localhost.localdomain>
2020-07-29 18:06:20 +0200

    [~] update carousel news order

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\PostClubRepository;
+use App\Repository\PostNatioRepository;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,21 +13,38 @@ class AcceuilController extends AbstractController
     /**
      * @Route("/", name="accueil")
      */
-    public function index(PostClubRepository $postClubRepository)
+    public function index(PostClubRepository $postClubRepository, PostNatioRepository $postNatioRepository)
     {
-        $posts = $postClubRepository->findBy([],['id'=>'DESC'],5,0);
-        $first = $posts[0];
-        $second = $posts[1];
-        $third = $posts[2];
-        $fourth = $posts[3];
-        $fifth = $posts[4];
+        $postsClub = $postClubRepository->findBy([], ['createdAt' => 'DESC'], 5, 0);
+        $postsNatio = $postNatioRepository->findBy([], ['createdAt' => 'DESC'], 5, 0);
+        $i = 0;
+        $y = 0;
+
+        $first = $postsClub[$i]->getCreatedAt() > $postsNatio[$y]->getCreatedAt() ? $postsClub[$i] : $postsNatio[$y];
+        if ($first === $postsClub[$i]) {
+            $i++;
+        } else {
+            $y++;
+        }
+        $second = $postsClub[$i]->getCreatedAt() > $postsNatio[$y]->getCreatedAt() ? $postsClub[$i] : $postsNatio[$y];
+        if ($second === $postsClub[$i]) {
+            $i++;
+        } else {
+            $y++;
+        }
+        $third = $postsClub[$i]->getCreatedAt() > $postsNatio[$y]->getCreatedAt() ? $postsClub[$i] : $postsNatio[$y];
+        if ($third === $postsClub[$i]) {
+            $i++;
+        } else {
+            $y++;
+        }
+        $fourth = $postsClub[$i]->getCreatedAt() > $postsNatio[$y]->getCreatedAt() ? $postsClub[$i] : $postsNatio[$y];
 
         return $this->render('acceuil/index.html.twig', [
             'first' => $first,
             'second' => $second,
             'third' => $third,
             'fourth' => $fourth,
-            'fifth' => $fifth,
         ]);
     }
 }
